4f32f649a4667214bea940a9f6a7f73b7ea7b160
Hayam <hi@hi.hi>
2026-07-01 17:34:06 +0530

    na

--- files/scripts/brave-origin.sh
+++ files/scripts/brave-origin.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 
-sudo dnf install dnf-plugins-core
-sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo dnf install brave-origin
+
+dnf5 install dnf-plugins-core
+dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+dnf5 install brave-origin
 
 
 
