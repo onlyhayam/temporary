1f620b1fb7b7918ac795d58d34ed983069dc0610
Hayam <hi@hi.hi>
2026-07-01 17:03:06 +0530

    debloated

---
+++ files/scripts/brave-origin.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo dnf install brave-origin
