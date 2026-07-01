eba7d5f7b9ace560ba4e5cc96673026e05ca64cd
Hayam <hi@hi.hi>
2026-07-01 23:01:58 +0530

    repo list cleared

--- files/scripts/brave-origin.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-rm -f /etc/yum.repos.d/*
--- files/scripts/repo.sh
+++ files/scripts/repo.sh
@@ -2,9 +2,4 @@
 
 
 
-dnf5 install dnf-plugins-core
-dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-dnf5 install brave-origin
-
-
-
+whoami
