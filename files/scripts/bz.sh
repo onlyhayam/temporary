5bf8d75910c51beb3c1429d93bb37d63b4064223
Hayam <hi@hi.hi>
2026-07-02 11:46:19 +0530

    bazaar chnged
    from yml to shell script

---
+++ files/scripts/bz.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+flatpak install -y --none-interactive flathub .io.kolumni.Bazaar
--- files/scripts/repo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-rm /etc/yum.repos.d/*
